3a8ee35d1d486f1f98f8a85a97a24a79fdfdfa52
青杨 <qingyang_882sina.com>
2021-07-17 22:25:46 +0800

    fix:路径

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
+import pkg from './package.json'
 
 export default defineConfig({
+    base: process.env.NODE_ENV !== 'production' ? '/' : '/' + pkg.name + '/',
     resolve: {
         alias: {
             
